ac4f5c6f387cd0618fd16fd6f2114dc897e74145
Dmitry Alexandrov <aleksandrov.d89@gmail.com>
2024-02-10 23:28:52 +0000

    nix flake start

---
+++ home-manager/home.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    homeDirectory =
+      if pkgs.stdenv.isLinux
+      then "/home/dimfatal"
+      else "/Users/dimfatal";
+
+    packages = with pkgs; [
+      bat
+      bloop
+      coursier
+      neovim
+    ];
+
+    stateVersion = "23.11";
+    username = "vlad";
+  };
+
+  programs.home-manager.enable = true;
+}
---
+++ home-manager/pkgs.nix
@@ -0,0 +1,56 @@
+nixpkgs: system: let
+  mangling = {
+    java = "graalvm-ce";
+  };
+
+  overlays = let
+    armOverlay = _: prev: let
+      pkgsForx86 = import nixpkgs {
+        localSystem = "x86_64-darwin";
+      };
+    in
+      prev.lib.optionalAttrs (prev.stdenv.isDarwin && prev.stdenv.isAarch64) {
+        inherit (pkgsForx86) bloop;
+      };
+
+    bloopOverlay = final: prev: {
+      bloop = prev.bloop.override {
+        jre = final.jre;
+      };
+    };
+
+    millOverlay = final: prev: {
+      mill = prev.mill.override {
+        jre = final.jre;
+      };
+    };
+
+    javaOverlay = final: _: {
+      jdk = final.${mangling.java};
+      jre = final.${mangling.java};
+    };
+
+    scalaCliOverlay = final: prev: {
+      scala-cli = prev.scala-cli.override {
+        # hardcoded because scala-cli requires 17 or above
+        jre = final.${mangling.java};
+      };
+    };
+  in [
+    armOverlay
+    bloopOverlay
+    javaOverlay
+    scalaCliOverlay
+    millOverlay
+  ];
+
+  pkgs = import nixpkgs {
+    inherit system overlays;
+
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+in
+  pkgs
